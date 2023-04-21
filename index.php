c3815e912404f89d58a2e96c3da41c457c994e67
prem18git <131329044+prem18git@users.noreply.github.com>
2023-04-21 14:03:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include "./includes/connection.php";
+
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BussBoss TN</title>
+</head>
+<body>
+    <div>
+    <form method="post">
+     <p>   <label>Arrival : <input type="text" name="arrival" required /></label>    </p> 
+     <p>    <label>Departure: <input type="text"  name="departure" required/></label>  </p>        
+     <p>  <button onclick= "./index/#answer" type="submit">Submit</button> </p>
+       </form>
+    </div>
+    <div id = answer><?php check(); ?></div>
+</body>
+</html>
